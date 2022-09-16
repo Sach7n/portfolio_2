eaf7e8c1d9bd5322f0ec8cfecd6f874a446e39b5
Sach7n <sachinmacwan07@gmail.com>
2022-09-16 18:50:34 -0400

    changed colors

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,29 +1,29 @@
 
     const first = [
     {
-        backgroundColor: "#3A3F55",
-        color: "#B0D7FF",
+        backgroundColor: "#fcfbfa",
+        color: "#4C0033",
         animation:"ease 1s",
         transition: "2s",
         height: "100%",
     },
     {
-        backgroundColor: "#274b59",
-        color: "#acdff2",
+       // backgroundColor: "#1A374D",
+        color: "#1A374D",
         animation:"ease 1s",
         transition: "2s", 
         height: "100%"
     },
     {
-        backgroundColor: "#441A2A",
-        color: "#f2bfcc",
+        //backgroundColor: "#191A19",
+        color: "#1C0C5B",
         animation:"ease 1s",
         transition: "2s", 
         height: "100%"
     },
     {
-        backgroundColor: "#042424",
-        color: "#eae8ff",
+        //backgroundColor: "#2E0249",
+        color: "#692501",
         animation:"ease 1s",
         transition: "2s",
         height: "100%"
